c79722f626c769ec4d92074d61a4e163964e6d1a
meloman1001 <58601223+meloman1001@users.noreply.github.com>
2019-12-12 22:26:34 +0300

    test

---
+++ vue.js
@@ -0,0 +1,58 @@
+
+const blocks=[{
+	id:1, 
+	name: "Заголовок"
+},
+{
+	id:2,
+	name: "Заголовок"
+},
+{
+	id:3, 
+	name: "Заголовок"
+},
+{
+	id:4, 
+	name: "Заголовок"
+},
+{
+	id:5, 
+	name: "Заголовок"
+},
+{
+	id:6, 
+	name: "Заголовок"
+},
+{
+	id:7, 
+	name: "Заголовок"
+},
+{
+	id:8, 
+	name: "Заголовок"
+}
+]
+
+let filterBlocks = blocks.filter(item=>{
+	if((item.id % 2) != 0 )return item;
+}
+		)
+
+new Vue({
+ el: '#cont',
+ data: {
+ 	mapBlocks:[],
+ 	width: 0
+ },
+  methods: {
+    updateMap() {
+      this.width = window.innerWidth;
+      window.innerWidth > 767?this.mapBlocks = blocks:this.mapBlocks = filterBlocks;
+            
+    },
+  },
+  created() {
+    window.addEventListener('resize', this.updateMap);
+    this.updateMap();
+  }
+})
